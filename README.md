cbe57d4c18a2bb906dff4b7a346ed6592922a682
talksik <patel.arjun50@gmail.com>
2024-01-27 10:25:17 -0800

    planning

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 1. run local server within network
 2. run client on different computers within network
+
+# Brainstorm
+Client should connect to websocket server. On connect, client receives a cache of most recent messages.
+
+On sending message, it is broadcasted to all connected clients, including sender.
+
--- client/src/Main.cpp
+++ client/src/Main.cpp
@@ -3,15 +3,10 @@
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-
-    // ask for name of person
     std::cout << "Please enter your first name: ";
     char *name = new char[100];
     std::cin >> name;
-
     std::string welcome_message = "Welcome, " + std::string(name) + "!";
-
     std::cout << std::endl << BOLDGREEN << welcome_message << RESET << std::endl;
 
     return 0;
